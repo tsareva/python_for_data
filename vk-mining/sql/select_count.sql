3e6bb41b2513d76a2ab2feb357cec312dc30539a
tsareva <atana@bastilia.ru>
2017-04-14 11:08:14 +0300

    Update select_count.sql

--- vk-mining/sql/select_count.sql
+++ vk-mining/sql/select_count.sql
@@ -1,4 +1,5 @@
-SELECT Groups.gid, Groups.name, Groups.type, Groups.is_closed, Groups.description, Group_count.count, (COUNT(Groups_members.user_id)*100/Group_count.count)
+SELECT Groups.gid, Groups.name, Groups.type, Groups.is_closed, Groups.description, 
+		Group_count.count, (COUNT(Groups_members.user_id)*100/Group_count.count)
 	FROM Groups
 		JOIN Group_count ON Groups.gid = Group_count.group_id
 		JOIN Groups_members ON Groups.gid = Groups_members.group_id
